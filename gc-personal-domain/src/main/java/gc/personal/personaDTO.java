31b59bc54aab1352716c0e0f46ae73341fce6332
claudioqg@gmail.com <claudioqg@gmail.com>
2019-11-28 15:01:10 +0100

    add initial project

---
+++ gc-personal-domain/src/main/java/gc/personal/personaDTO.java
@@ -0,0 +1,14 @@
+
+package gc.personal;
+
+
+public class personaDTO {
+    private Integer id;    
+    private String Name;
+    private final Object name;
+
+    public personaDTO() {
+        this.id = null;
+        this.name = null;
+    }
+}
---
+++ gc-personal-ejb/src/main/java/gc/personal/personaEJB.java
@@ -0,0 +1,10 @@
+
+package gc.personal;
+
+public class personaEJB {
+    public personaDTO buscarpersonapornombre() {
+        return new personaDTO ();
+      
+    }
+    
+}
